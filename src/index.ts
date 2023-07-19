6e39e59f147a5e539a9922a6ed7bd512adbba0c3
Carl Ansley <carl.ansley@gmail.com>
2023-07-19 16:53:02 -0400

    Switch to tag-release publishing

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-// index.ts
-
-// This file is intentionally empty. As custom linting rules are needed, they will be added to this src directory.
-
-// eslint-disable-next-line eslint-comments/disable-enable-pair
-/* eslint-disable unicorn/no-empty-file */
